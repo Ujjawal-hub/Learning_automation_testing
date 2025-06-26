b2650bb1ab0b5d83a0bb48f20e091b04280921eb
Ujjawal-hub <jainujjawal22@gmail.com>
2025-06-26 16:39:23 +0530

    newfiles

---
+++ src/test1/_1.java
@@ -0,0 +1,19 @@
+package test1;
+
+public class _1 {
+
+    public static void main(String[] args) {
+        //Integer: 25 Double: 3.14 Boolean: true Character: A
+
+        int a=25;
+        double b= 3.14;
+        boolean c= true;
+        char  d= 'A';
+
+        System.out.println("Integer: " + a);
+        System.out.println("Double: "+ b);
+        System.out.println("Boolean: " + c);
+        System.out.println("Charcter: "+d);
+
+    }
+}
