356e733b28d7ed5dd6b02111196dfc525d637bc0
Ujjawal-hub <jainujjawal22@gmail.com>
2025-06-27 13:20:35 +0530

    newfiles

---
+++ src/fuctions/question.java
@@ -0,0 +1,23 @@
+package fuctions;
+
+public class question {
+    public static void main(String[] args) {
+
+      boolean a= main(false);
+        System.out.println(a);
+    }
+    public static void main(String args) {
+        System.out.println("Yes");
+    }
+    public static int main(int args) {
+        return 10;
+    }
+
+    public static int main(float args) {
+        return 10;
+    }
+
+    public static boolean main(boolean args) {
+        return true;
+    }
+}
